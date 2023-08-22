94607310556e45c1635d82b8dc141183dc911a21
Ezgi <107739024+ezgi-c@users.noreply.github.com>
2023-08-21 22:19:51 -0700

    built-in data structures: string methods and lists

---
+++ built-in_data_structures/string_methods.py
@@ -0,0 +1,15 @@
+intro = "My name is Ezgi!"
+
+print(intro[0])
+print(intro[0:2]) # prints first 2 letters 
+print(intro[-5:-1]) # prints the letters between 1 and 5 indices from the back
+
+print(len(intro))
+
+intro.lower()
+intro.upper()
+intro.title()
+
+print(intro.split)
+print(intro.split('name'))
+print(intro.split("!"))
